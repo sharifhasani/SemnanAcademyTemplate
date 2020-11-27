318d44338cb1d79ede7bc3fd085c2935b5a65dc6
sharif hasani <sharif.hasani.1379@outlook.com>
2020-11-27 23:28:25 +0330

    complete header, landing and ads banner with responsive design

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function ShowResponsiveMenu(element = document.getElementById("menu-icon")) {
 
     let responsiveMenu = document.getElementById("responsive-menu");
 
+    //hide responsive menu
     if (responsiveMenu.style.right == "0px") {
 
         let backContainer = document.getElementsByClassName("back-container");
@@ -10,7 +11,14 @@ function ShowResponsiveMenu(element = document.getElementById("menu-icon")) {
         element.classList.add("zmdi-menu");
         element.classList.remove("zmdi-close");
         responsiveMenu.style.right = "-220px";
-    } else {
+
+        //enable scroll
+        document.getElementsByTagName("body")[0].style.height = "100%";
+        document.getElementsByTagName("body")[0].style.overflow = "auto";
+
+    }
+    //show responsive menu
+    else {
         let backContainer = document.createElement("div");
         backContainer.classList.add("back-container");
 
@@ -20,5 +28,24 @@ function ShowResponsiveMenu(element = document.getElementById("menu-icon")) {
         element.classList.add("zmdi-close");
         element.classList.remove("zmdi-menu");
         responsiveMenu.style.right = "0px";
+
+        //disable scroll
+        document.getElementsByTagName("body")[0].style.height = "100%";
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
     }
+}
+
+function ShowSubMenu(elementId) {
+    document.getElementById(elementId).style.visibility = "visible";
+    document.getElementById(elementId).style.opacity = "1";
+    document.getElementById("sub-" + elementId).style.display = "flex";
+
+
+}
+
+function HideSubMenu(elementId) {
+
+    document.getElementById(elementId).style.visibility = "hidden";
+    document.getElementById(elementId).style.opacity = "0.8";
+    document.getElementById("sub-" + elementId).style.display = "none";
 }
